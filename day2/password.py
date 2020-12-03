88fc9d143bbc4b52f8b6a41322c34fa90722ee61
Matt Porter <matthew.js.porter@gmail.com>
2020-12-02 22:50:06 -0500

    completed Day 2 part 2

--- day2/password.py
+++ day2/password.py
@@ -1,16 +1,17 @@
 import re
 
 
-def is_valid(password: str, policy_character, min: int, max: int) -> bool:
-    return min <= len(re.findall(policy_character, password)) <= max
+def is_valid(password: str, policy_character, first: int, second: int) -> bool:
+    return (password[first-1] == policy_character) != (password[second-1] == policy_character)
+
 
 def is_valid_policy(password_policy: str) -> bool:
     policy_attributes = password_policy.split()
-    min = int(policy_attributes[0].split("-")[0])
-    max = int(policy_attributes[0].split("-")[1])
+    first = int(policy_attributes[0].split("-")[0])
+    second = int(policy_attributes[0].split("-")[1])
     policy_character = policy_attributes[1].replace(":", "")
     password = policy_attributes[2]
-    return is_valid(password, policy_character, min, max)
+    return is_valid(password, policy_character, first, second)
 
 
 if __name__ == '__main__':
--- day2/test_password.py
+++ day2/test_password.py
@@ -7,23 +7,23 @@ class MyTestCase(unittest.TestCase):
     def test_valid_password(self):
         policy_character = 'a'
         user_password = 'abcde'
-        min = 1
-        max = 3
-        self.assertTrue(password.is_valid(user_password, policy_character, min, max))
+        first = 1
+        second = 3
+        self.assertTrue(password.is_valid(user_password, policy_character, first, second))
 
     def test_invalid_password(self):
         policy_character = 'b'
         user_password = 'cdefg'
-        min = 1
-        max = 3
-        self.assertFalse(password.is_valid(user_password, policy_character, min, max))
+        first = 1
+        second = 3
+        self.assertFalse(password.is_valid(user_password, policy_character, first, second))
 
     def test_valid_same_char(self):
         policy_character = 'c'
         user_password = 'ccccccccc'
-        min = 2
-        max = 9
-        self.assertTrue(password.is_valid(user_password, policy_character, min, max))
+        first = 2
+        second = 9
+        self.assertFalse(password.is_valid(user_password, policy_character, first, second))
 
     def test_valid_policy(self):
         policy = '1-3 a: abcde'
