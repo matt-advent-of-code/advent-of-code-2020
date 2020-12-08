4dff767022d771bdb430b09fe58a44b46144bca0
Matt Porter <matthew.js.porter@gmail.com>
2020-12-08 01:59:56 -0500

    Completed day 7 part 2

--- day8/game.py
+++ day8/game.py
@@ -11,57 +11,92 @@ instruction_stack = []
 
 
 def parse(data: str) -> list:
-    return [(instruction.split(" ")[0], int(instruction.split(" ")[1])) for instruction in data.splitlines()]
+    return [[instruction.split(" ")[0], int(instruction.split(" ")[1])] for instruction in data.splitlines()]
 
 
-class NoopInstruction:
-    def execute(self, arg: int):
-        global instruction_index
-        instruction_index += 1
+class Game:
+    def __init__(self):
+        self.accumulator = 0
+        self.instruction_index = 0
+        self.instruction_stack = []
+
+    def boot(self, instructions: list):
+        while self.instruction_index < len(instructions):
+            instruction = instructions[self.instruction_index]
+            bi = self.build_instruction(instruction[0])
+            self.instruction_stack.append(self.instruction_index)
+            bi.execute(instruction[1])
+
+    class NoopInstruction:
+        def __init__(self, game):
+            self.game = game
+
+        def execute(self, arg: int):
+            self.game.instruction_index += 1
+
+    class AccumulatorInstruction:
+        def __init__(self, game):
+            self.game = game
+
+        def execute(self, arg: int):
+            self.game.accumulator += arg
+            self.game.instruction_index += 1
+
+    class JumpInstruction(NoopInstruction):
+        def __init__(self, game):
+            self.game = game
+
+        def execute(self, arg: int):
+            self.game.instruction_index += arg
+
+    def build_instruction(self, name: str):
+        if self.instruction_index in self.instruction_stack:
+            raise Exception
+        elif name == 'nop':
+            return Game.NoopInstruction(self)
+        elif name == 'acc':
+            return Game.AccumulatorInstruction(self)
+        elif name == 'jmp':
+            return Game.JumpInstruction(self)
+        else:
+            raise Exception
 
 
-class AccumulatorInstruction(NoopInstruction):
-    def execute(self, arg: int):
-        super(AccumulatorInstruction, self).execute(arg)
-        global accumulator
-        accumulator += arg
+class AlteringGame(Game):
+    def boot(self, instructions: list):
+        while self.instruction_index < len(instructions):
+            instruction = instructions[self.instruction_index]
+            if instruction[0] == 'nop' or instruction[0] == 'jmp':
+                new_game = Game()
+                new_game.accumulator = self.accumulator
+                new_game.instruction_index = self.instruction_index
+                new_game.instruction_stack = self.instruction_stack.copy()
+                if instruction[0] == 'nop':
+                    instructions[self.instruction_index][0] = 'jmp'
+                    old_instruction = 'nop'
+                else:
+                    instructions[self.instruction_index][0] = 'nop'
+                    old_instruction = 'jmp'
 
+                try:
+                    new_game.boot(instructions)
+                    print(new_game.accumulator)
+                    exit()
+                except Exception as e:
+                    instructions[self.instruction_index][0] = old_instruction
 
-class JumpInstruction(NoopInstruction):
-    def execute(self, arg: int):
-        global instruction_index
-        instruction_index += arg
-
-
-class BreakInstruction(NoopInstruction):
-    def execute(self, arg: int):
-        global end
-        end = True
-
-
-def boot(instructions: list):
-    while not end:
-        instruction = instructions[instruction_index]
-        bi = build_instruction(instruction[0])
-        instruction_stack.append(instruction_index)
-        bi.execute(instruction[1])
-
-
-def build_instruction(name: str):
-    if instruction_index in instruction_stack and len(instruction_stack) != 1:
-        return BreakInstruction()
-    elif name == 'nop':
-        return NoopInstruction()
-    elif name == 'acc':
-        return AccumulatorInstruction()
-    elif name == 'jmp':
-        return JumpInstruction()
-    else:
-        raise Exception
+            instruction = instructions[self.instruction_index]
+            bi = self.build_instruction(instruction[0])
+            self.instruction_stack.append(self.instruction_index)
+            bi.execute(instruction[1])
 
 
 if __name__ == '__main__':
     with open('input.txt') as data:
         instructions = parse(data.read())
-        boot(instructions)
-        print(accumulator)
+        game = AlteringGame()
+        try:
+            game.boot(instructions)
+        except Exception as e:
+            print(game.accumulator)
+            raise e
