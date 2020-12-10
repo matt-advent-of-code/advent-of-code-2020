68184792a00a2e2aaac805c1b7a8ff5ee98a86e9
Matt Porter <matthew.js.porter@gmail.com>
2020-12-10 14:37:42 -0500

    working "solution" that is too slow

--- day10/adapter.py
+++ day10/adapter.py
@@ -1,11 +1,7 @@
 def find_diffs(adapters: list) -> tuple:
     diffs = {}
-    sorted_adapters = adapters.copy()
-    sorted_adapters.append(0)
-    sorted_adapters.sort()
-    sorted_adapters.append(sorted_adapters[-1] + 3)
-    for i in range(1, len(sorted_adapters)):
-        diff = sorted_adapters[i] - sorted_adapters[i - 1]
+    for i in range(1, len(adapters)):
+        diff = adapters[i] - adapters[i - 1]
         if diff in diffs:
             diffs[diff] += 1
         else:
@@ -13,8 +9,30 @@ def find_diffs(adapters: list) -> tuple:
     return diffs
 
 
+def find_arrangements(adapters: list) -> int:
+    sorted_adapters = adapters.copy()
+    sorted_adapters.append(0)
+    sorted_adapters.sort()
+    sorted_adapters.append(sorted_adapters[-1] + 3)
+    return __find_arrangements(sorted_adapters, 1)
+
+
+def __find_arrangements(adapters: list, start: int) -> int:
+    sorted_adapters = adapters.copy()
+    sorted_adapters.sort()
+    number_of_arrangements = 0
+    diffs = find_diffs(adapters)
+    if max(list(diffs.keys())) <= 3:
+        for i in range(start, len(adapters) - 1):
+            adapters_copy = sorted_adapters.copy()
+            adapters_copy.pop(i)
+            number_of_arrangements += __find_arrangements(adapters_copy, i)
+        return 1 + number_of_arrangements
+    else:
+        return 0
+
+
 if __name__ == '__main__':
     with open('input.txt') as data:
         adapters = [int(adapter) for adapter in data.readlines()]
-        diffs = find_diffs(adapters)
-        print(diffs[1] * diffs[3])
+        print(find_arrangements(adapters))
--- day10/test_adapter.py
+++ day10/test_adapter.py
@@ -17,6 +17,20 @@ class MyTestCase(unittest.TestCase):
         diff = adapter.find_diffs(adapters)
         self.assertEqual(expected_diff, diff)
 
+    def test_number_possible_arangements(self):
+        adapters = [16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4]
+        expected_number_of_arangements = 8
+        number_of_aranagements = adapter.find_arrangements(adapters)
+        self.assertEqual(expected_number_of_arangements, number_of_aranagements)
+
+
+    def test_number_possible_arangements(self):
+        adapters = [28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19, 38, 39, 11, 1, 32, 25, 35, 8, 17,
+                    7, 9, 4, 2, 34, 10, 3]
+        expected_number_of_arangements = 19208
+        number_of_aranagements = adapter.find_arrangements(adapters)
+        self.assertEqual(expected_number_of_arangements, number_of_aranagements)
+
 
 if __name__ == '__main__':
     unittest.main()
