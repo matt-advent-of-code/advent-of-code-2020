6ff45004fc056b009ed0d85e7894a6ed92423c00
Matt Porter <matthew.js.porter@gmail.com>
2020-12-10 23:16:30 -0500

    finally a working solution

--- day10/adapter.py
+++ day10/adapter.py
@@ -1,5 +1,6 @@
 import networkx as nx
 
+
 def find_diffs(adapters: list) -> tuple:
     diffs = {}
     for i in range(1, len(adapters)):
@@ -12,27 +13,22 @@ def find_diffs(adapters: list) -> tuple:
 
 
 def find_arrangements(adapters: list) -> int:
-    graph = to_graph(adapters)
-    diGraph = nx.DiGraph()
-    diGraph.add_edges_from(graph)
-    return len(list(nx.all_simple_paths(diGraph, graph[0][0], graph[-1][0])))
-
-
-def to_graph(adapters: list) -> list:
     sorted_adapters = adapters.copy()
     sorted_adapters.append(0)
     sorted_adapters.sort()
     sorted_adapters.append(sorted_adapters[-1] + 3)
-    print(sorted_adapters)
 
-    graph = []
-    for i in range(len(sorted_adapters) -1):
-        j=i+1
-        while j < len(sorted_adapters) and sorted_adapters[j] - sorted_adapters[i] <=3:
-            graph.append((sorted_adapters[i], sorted_adapters[j]))
-            j+=1
-    print(graph)
-    return graph
+    paths = {0: 1}
+    for i in range(1, len(sorted_adapters)):
+        j = i - 1
+        number_of_paths = 0
+        while j >= 0 and sorted_adapters[i] - sorted_adapters[j] <= 3:
+            number_of_paths += paths[sorted_adapters[j]]
+            j = j - 1
+        paths[sorted_adapters[i]] = number_of_paths
+
+    return paths[sorted_adapters[-1]]
+
 
 if __name__ == '__main__':
     with open('input.txt') as data:
