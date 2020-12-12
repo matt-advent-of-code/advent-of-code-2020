7d92225fa30ab646f111104873f67169f2089deb
Matt Porter <matthew.js.porter@gmail.com>
2020-12-12 10:30:40 -0500

    Day 12 Part 2

--- day12/ferry.py
+++ day12/ferry.py
@@ -5,33 +5,31 @@ class Action:
 
 class NorthAction(Action):
     def act(self, action: str, ship):
-        ship.coordinates = (ship.coordinates[0], ship.coordinates[1] + int(action[1:]))
+        ship.waypoint = (ship.waypoint[0], ship.waypoint[1] + int(action[1:]))
 
 
 class SouthAction(Action):
     def act(self, action: str, ship):
-        ship.coordinates = (ship.coordinates[0], ship.coordinates[1] - int(action[1:]))
+        ship.waypoint = (ship.waypoint[0], ship.waypoint[1] - int(action[1:]))
 
 
 class EastAction(Action):
     def act(self, action: str, ship):
-        ship.coordinates = (ship.coordinates[0] + int(action[1:]), ship.coordinates[1])
+        ship.waypoint = (ship.waypoint[0] + int(action[1:]), ship.waypoint[1])
 
 
 class WestAction(Action):
     def act(self, action: str, ship):
-        ship.coordinates = (ship.coordinates[0] - int(action[1:]), ship.coordinates[1])
+        ship.waypoint = (ship.waypoint[0] - int(action[1:]), ship.waypoint[1])
 
 
 class TurnRightAction(Action):
     def act(self, action: str, ship):
-        direction = Ship.directions[ship.direction]
-        degree = int(action[1:])
-        degree = simplify_degree(degree)
-        new_direction = simplify_degree(direction + degree)
-        for key, value in Ship.directions.items():
-            if value == new_direction:
-                ship.direction = key
+        for degree in range((int(action[1:])), 0, -90):
+            relative_waypoint = calculate_relative_waypoint(ship.waypoint, ship.coordinates)
+            rotated_relative_waypoint = (relative_waypoint[1], -1 * relative_waypoint[0])
+            ship.waypoint = (ship.coordinates[0] + rotated_relative_waypoint[0], ship.coordinates[1]
+                             + rotated_relative_waypoint[1])
 
 
 def simplify_degree(degree: int):
@@ -47,21 +45,23 @@ class TurnLeftAction(TurnRightAction):
 
 class ForwardDirection(Action):
     def act(self, action: str, ship):
-        action = ship.direction + action[1:]
-        Ship.get_action(action).act(action, ship)
+        multiplier = int(action[1:])
+        relative_waypoint = calculate_relative_waypoint(ship.waypoint, ship.coordinates)
+        x = multiplier * relative_waypoint[0] + ship.coordinates[0]
+        y = multiplier * relative_waypoint[1] + ship.coordinates[1]
+        ship.coordinates = x, y
+        ship.waypoint = ship.coordinates[0] + relative_waypoint[0], ship.coordinates[1] + relative_waypoint[1]
+
+
+def calculate_relative_waypoint(waypoint: tuple, coordinates: tuple):
+    return waypoint[0] - coordinates[0], waypoint[1] - coordinates[1]
 
 
 class Ship:
-    directions = {
-        'E': 0,
-        'S': 90,
-        'W': 180,
-        'N': 270
-    }
 
-    def __init__(self, coordinates: tuple, direction: str):
+    def __init__(self, coordinates: tuple, waypoint: tuple):
         self.coordinates = coordinates
-        self.direction = direction
+        self.waypoint = waypoint
 
     def move(self, action: str):
         Ship.get_action(action).act(action, self)
@@ -88,7 +88,7 @@ class Ship:
 
 if __name__ == '__main__':
     with open('input.txt') as data:
-        ship = Ship((0, 0), 'E')
+        ship = Ship((0, 0), (10,1))
         for direction in data.readlines():
             ship.move(direction)
 
--- day12/test_ferry.py
+++ day12/test_ferry.py
@@ -5,88 +5,95 @@ from day12.ferry import Ship
 
 class MyTestCase(unittest.TestCase):
     def test_ferry(self):
-        ship = Ship((0, 0), 'E')
+        ship = Ship((0, 0), (10, 1))
         self.assertEqual((0, 0), ship.coordinates)
+        self.assertEqual((10, 1), ship.waypoint)
 
     def test_move_north(self):
-        ship = Ship((0, 0), 'E')
+        ship = Ship((0, 0), (10, 1))
         ship.move('N3')
-        self.assertEqual((0, 3), ship.coordinates)
+        self.assertEqual((0, 0), ship.coordinates)
+        self.assertEqual((10, 4), ship.waypoint)
 
     def test_move_south(self):
-        ship = Ship((0, 0), 'E')
+        ship = Ship((0, 0), (10, 1))
         ship.move('S3')
-        self.assertEqual((0, -3), ship.coordinates)
+        self.assertEqual((0, 0), ship.coordinates)
+        self.assertEqual((10, -2), ship.waypoint)
 
     def test_move_east(self):
-        ship = Ship((0, 0), 'E')
+        ship = Ship((0, 0), (10, 1))
         ship.move('E3')
-        self.assertEqual((3, 0), ship.coordinates)
+        self.assertEqual((0, 0), ship.coordinates)
+        self.assertEqual((13, 1), ship.waypoint)
 
     def test_move_west(self):
-        ship = Ship((0, 0), 'E')
+        ship = Ship((0, 0), (10, 1))
         ship.move('W3')
-        self.assertEqual((-3, 0), ship.coordinates)
+        self.assertEqual((0, 0), ship.coordinates)
+        self.assertEqual((7, 1), ship.waypoint)
 
     def test_move_forward(self):
-        ship = Ship((0, 0), 'E')
+        ship = Ship((0, 0), (10, 1))
         ship.move('F10')
-        self.assertEqual((10, 0), ship.coordinates)
+        self.assertEqual((100, 10), ship.coordinates)
+        self.assertEqual((110, 11), ship.waypoint)
+
+    def test_move_negative(self):
+        ship = Ship((0, 0), (-10, -1))
+        ship.move('F10')
+        self.assertEqual((-100, -10), ship.coordinates)
+        self.assertEqual((-110, -11), ship.waypoint)
 
     def test_right_90(self):
-        ship = Ship((0, 0), 'E')
+        ship = Ship((170, 38), (180, 42))
         ship.move('R90')
-        self.assertEqual('S', ship.direction)
+        self.assertEqual((174, 28), ship.waypoint)
 
     def test_right_180(self):
-        ship = Ship((0, 0), 'E')
+        ship = Ship((0, 0), (1, 0))
         ship.move('R180')
-        self.assertEqual('W', ship.direction)
+        self.assertEqual((-1, 0), ship.waypoint)
 
     def test_right_270(self):
-        ship = Ship((0, 0), 'E')
+        ship = Ship((0, 0), (1, 0))
         ship.move('R270')
-        self.assertEqual('N', ship.direction)
+        self.assertEqual((0, 1), ship.waypoint)
 
     def test_right_450(self):
-        ship = Ship((0, 0), 'E')
+        ship = Ship((0, 0), (1, 0))
         ship.move('R450')
-        self.assertEqual('S', ship.direction)
+        self.assertEqual((0, -1), ship.waypoint)
 
     def test_left_90(self):
-        ship = Ship((0, 0), 'E')
+        ship = Ship((0, 0), (1, 0))
         ship.move('L90')
-        self.assertEqual('N', ship.direction)
-
-    def test_left_90_north(self):
-        ship = Ship((0, 0), 'N')
-        ship.move('L90')
-        self.assertEqual('W', ship.direction)
+        self.assertEqual((0, 1), ship.waypoint)
 
     def test_left_180(self):
-        ship = Ship((0, 0), 'E')
+        ship = Ship((0, 0), (1, 0))
         ship.move('L180')
-        self.assertEqual('W', ship.direction)
+        self.assertEqual((-1, 0), ship.waypoint)
 
     def test_left_270(self):
-        ship = Ship((0, 0), 'E')
+        ship = Ship((0, 0), (1, 0))
         ship.move('L270')
-        self.assertEqual('S', ship.direction)
+        self.assertEqual((0, -1), ship.waypoint)
 
     def test_left_450(self):
-        ship = Ship((0, 0), 'E')
+        ship = Ship((0, 0), (1, 0))
         ship.move('L450')
-        self.assertEqual('N', ship.direction)
+        self.assertEqual((0, 1), ship.waypoint)
 
     def test_move(self):
-        ship = Ship((0, 0), 'E')
+        ship = Ship((0, 0), (10, 1))
         ship.move('F10')
         ship.move('N3')
         ship.move('F7')
         ship.move('R90')
         ship.move('F11')
 
-        self.assertEqual((17, -8), ship.coordinates)
+        self.assertEqual((214, -72), ship.coordinates)
 
 
 if __name__ == '__main__':
