909a2ca73afc9292840bc8da7d4d42f465d0dcee
Matt Porter <matthew.js.porter@gmail.com>
2020-12-19 10:25:55 -0500

    updating input and test

--- day19/test_match.py
+++ day19/test_match.py
@@ -47,19 +47,72 @@ class MyTestCase(unittest.TestCase):
 
 
     def test_sample(self):
-        rules = """0: 4 1 5
-1: 2 3 | 3 2
-2: 4 4 | 5 5
-3: 4 5 | 5 4
-4: "a"
+        rules = """0: 8
+8: 42 | 42 8
+31: 14 17 | 1 13
+42: 9 14 | 10 1
+14: "a"
+9: "b"
+10: "a"
+1: "a"
 5: "b"""
 
-        self.assertTrue(match.matches(rules, 'ababbb', 0))
-        self.assertTrue(match.matches(rules, 'abbbab', 0))
+        self.assertTrue(match.matches(rules, 'a', 0))
 
-        self.assertFalse(match.matches(rules, 'bababa', 0))
-        self.assertFalse(match.matches(rules, 'aaabbb', 0))
-        self.assertFalse(match.matches(rules, 'aaaabbb', 0))
+
+
+    def test_small_loop(self):
+        rules = """0: 1 2
+        1: "a"
+        2: 1 2 4 | 3
+        3: "b\"
+        4: 2 1"""
+        self.assertTrue(match.matches(rules, 'aabba', 0))
+
+    def test_loop(self):
+        rules = """42: 9 14 | 10 1
+9: 14 27 | 1 26
+10: 23 14 | 28 1
+1: "a"
+11: 42 31 | 42 11 31
+5: 1 14 | 15 1
+19: 14 1 | 14 14
+12: 24 14 | 19 1
+16: 15 1 | 14 14
+31: 14 17 | 1 13
+6: 14 14 | 1 14
+2: 1 24 | 14 4
+0: 8 11
+13: 14 3 | 1 12
+15: 1 | 14
+17: 14 2 | 1 7
+23: 25 1 | 22 14
+28: 16 1
+4: 1 1
+20: 14 14 | 1 15
+3: 5 14 | 16 1
+27: 1 6 | 14 18
+14: "b"
+21: 14 1 | 1 14
+25: 1 1 | 1 14
+22: 14 14
+8: 42 | 42 8
+26: 14 22 | 1 20
+18: 15 15
+7: 14 5 | 1 21
+24: 14 1"""
+        self.assertTrue(match.matches(rules, 'bbabbbbaabaabba', 0))
+        self.assertTrue(match.matches(rules, 'babbbbaabbbbbabbbbbbaabaaabaaa', 0))
+        self.assertTrue(match.matches(rules, 'aaabbbbbbaaaabaababaabababbabaaabbababababaaa', 0))
+#        self.assertTrue(match.matches(rules, 'bbbbbbbaaaabbbbaaabbabaaa', 0))
+#        self.assertTrue(match.matches(rules, 'bbbababbbbaaaaaaaabbababaaababaabab', 0))
+        self.assertTrue(match.matches(rules, 'ababaaaaaabaaab', 0))
+        self.assertTrue(match.matches(rules, 'ababaaaaabbbaba', 0))
+        self.assertTrue(match.matches(rules, 'baabbaaaabbaaaababbaababb', 0))
+#        self.assertTrue(match.matches(rules, 'abbbbabbbbaaaababbbbbbaaaababb', 0))
+#        self.assertTrue(match.matches(rules, 'aaaaabbaabaaaaababaa', 0))
+        self.assertTrue(match.matches(rules, 'aaaabbaabbaaaaaaabbbabbbaaabbaabaaa', 0))
+#        self.assertTrue(match.matches(rules, 'aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba', 0))
 
 
 
