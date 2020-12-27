eb01bb45f331074a949e9f06abd7bf5b3c4fa34c
Matt Porter <matthew.js.porter@gmail.com>
2020-12-27 14:26:28 -0500

    Day 24 Part 2

--- day24/lobby.py
+++ day24/lobby.py
@@ -1,8 +1,10 @@
 import re
-def flip(layout: str) -> int:
-    black_tiles = []
+
+
+def seed(layout: str) -> set:
+    black_tiles = set()
     for tile in layout.split('\n'):
-        start = (0,0)
+        start = (0, 0)
         location = start
         moves = re.findall(r'(e|se|sw|w|nw|ne)', tile)
         for move in moves:
@@ -24,11 +26,55 @@ def flip(layout: str) -> int:
         if location in black_tiles:
             black_tiles.remove(location)
         else:
-            black_tiles.append(location)
+            black_tiles.add(location)
+
+    return black_tiles
+
+
+def flip(black_tiles: set, days: int) -> int:
+    for _ in range(days):
+        flip_one_day(black_tiles)
 
     return len(black_tiles)
 
 
+def flip_one_day(black_tiles: set):
+    black_tiles_copy = black_tiles.copy()
+    for black_tile in black_tiles_copy:
+        neighbors = get_neighbors(black_tile)
+        black_neighbors = 0
+        for neighbor in neighbors:
+            if neighbor in black_tiles_copy:
+                black_neighbors += 1
+        if black_neighbors == 0 or black_neighbors > 2:
+            black_tiles.remove(black_tile)
+
+    for black_tile in black_tiles_copy:
+        neighbors = get_neighbors(black_tile)
+        for neighbor in neighbors:
+            if neighbor not in black_tiles_copy:
+                while_tile = neighbor
+                white_tile_neighbors = get_neighbors(while_tile)
+                black_neighbors = 0
+                for white_tile_neighbor in white_tile_neighbors:
+                    if white_tile_neighbor in black_tiles_copy:
+                        black_neighbors += 1
+                if black_neighbors == 2:
+                    black_tiles.add(while_tile)
+
+
+def get_neighbors(tile: tuple) -> list:
+    return [
+        (tile[0], tile[1] - 1),
+        (tile[0] - 1, tile[1]),
+        (tile[0] - 1, tile[1] + 1),
+        (tile[0], tile[1] + 1),
+        (tile[0] + 1, tile[1]),
+        (tile[0] + 1, tile[1] - 1)
+    ]
+
+
 if __name__ == '__main__':
     with open('input.txt') as data:
-        print(flip(data.read()))
+        black_tiles = seed(data.read())
+        print(flip(black_tiles, 100))
--- day24/test_lobby.py
+++ day24/test_lobby.py
@@ -4,7 +4,7 @@ from day24 import lobby
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
+    def test_seed(self):
         layout = """sesenwnenenewseeswwswswwnenewsewsw
 neeenesenwnwwswnenewnwwsewnenwseswesw
 seswneswswsenwwnwse
@@ -27,8 +27,95 @@ neswnwewnwnwseenwseesewsenwsweewe
 wseweeenwnesenwwwswnew"""
 
         expected_black_tiles = 10
-        black_tiles = lobby.flip(layout)
-        self.assertEqual(expected_black_tiles, black_tiles)
+        black_tiles = lobby.seed(layout)
+        self.assertEqual(expected_black_tiles, len(black_tiles))
+
+
+    def test_flip_one_day(self):
+        layout = """sesenwnenenewseeswwswswwnenewsewsw
+neeenesenwnwwswnenewnwwsewnenwseswesw
+seswneswswsenwwnwse
+nwnwneseeswswnenewneswwnewseswneseene
+swweswneswnenwsewnwneneseenw
+eesenwseswswnenwswnwnwsewwnwsene
+sewnenenenesenwsewnenwwwse
+wenwwweseeeweswwwnwwe
+wsweesenenewnwwnwsenewsenwwsesesenwne
+neeswseenwwswnwswswnw
+nenwswwsewswnenenewsenwsenwnesesenew
+enewnwewneswsewnwswenweswnenwsenwsw
+sweneswneswneneenwnewenewwneswswnese
+swwesenesewenwneswnwwneseswwne
+enesenwswwswneneswsenwnewswseenwsese
+wnwnesenesenenwwnenwsewesewsesesew
+nenewswnwewswnenesenwnesewesw
+eneswnwswnwsenenwnwnwwseeswneewsenese
+neswnwewnwnwseenwseesewsenwsweewe
+wseweeenwnesenwwwswnew"""
+        black_tiles = lobby.seed(layout)
+
+        expected_number_of_black_tiles = 15
+        number_of_black_tiles = lobby.flip(black_tiles, 1)
+
+        self.assertEqual(expected_number_of_black_tiles, number_of_black_tiles)
+
+
+    def test_flip_two_days(self):
+        layout = """sesenwnenenewseeswwswswwnenewsewsw
+neeenesenwnwwswnenewnwwsewnenwseswesw
+seswneswswsenwwnwse
+nwnwneseeswswnenewneswwnewseswneseene
+swweswneswnenwsewnwneneseenw
+eesenwseswswnenwswnwnwsewwnwsene
+sewnenenenesenwsewnenwwwse
+wenwwweseeeweswwwnwwe
+wsweesenenewnwwnwsenewsenwwsesesenwne
+neeswseenwwswnwswswnw
+nenwswwsewswnenenewsenwsenwnesesenew
+enewnwewneswsewnwswenweswnenwsenwsw
+sweneswneswneneenwnewenewwneswswnese
+swwesenesewenwneswnwwneseswwne
+enesenwswwswneneswsenwnewswseenwsese
+wnwnesenesenenwwnenwsewesewsesesew
+nenewswnwewswnenesenwnesewesw
+eneswnwswnwsenenwnwnwwseeswneewsenese
+neswnwewnwnwseenwseesewsenwsweewe
+wseweeenwnesenwwwswnew"""
+        black_tiles = lobby.seed(layout)
+
+        expected_number_of_black_tiles = 12
+        number_of_black_tiles = lobby.flip(black_tiles, 2)
+
+        self.assertEqual(expected_number_of_black_tiles, number_of_black_tiles)
+
+
+    def test_flip_100_days(self):
+        layout = """sesenwnenenewseeswwswswwnenewsewsw
+neeenesenwnwwswnenewnwwsewnenwseswesw
+seswneswswsenwwnwse
+nwnwneseeswswnenewneswwnewseswneseene
+swweswneswnenwsewnwneneseenw
+eesenwseswswnenwswnwnwsewwnwsene
+sewnenenenesenwsewnenwwwse
+wenwwweseeeweswwwnwwe
+wsweesenenewnwwnwsenewsenwwsesesenwne
+neeswseenwwswnwswswnw
+nenwswwsewswnenenewsenwsenwnesesenew
+enewnwewneswsewnwswenweswnenwsenwsw
+sweneswneswneneenwnewenewwneswswnese
+swwesenesewenwneswnwwneseswwne
+enesenwswwswneneswsenwnewswseenwsese
+wnwnesenesenenwwnenwsewesewsesesew
+nenewswnwewswnenesenwnesewesw
+eneswnwswnwsenenwnwnwwseeswneewsenese
+neswnwewnwnwseenwseesewsenwsweewe
+wseweeenwnesenwwwswnew"""
+        black_tiles = lobby.seed(layout)
+
+        expected_number_of_black_tiles = 2208
+        number_of_black_tiles = lobby.flip(black_tiles, 100)
+
+        self.assertEqual(expected_number_of_black_tiles, number_of_black_tiles)
 
 
 if __name__ == '__main__':
