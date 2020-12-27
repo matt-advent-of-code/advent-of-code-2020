7a7b5fa1d7397b3cfb59ec8dae7d6fcd569cebea
Matt Porter <matthew.js.porter@gmail.com>
2020-12-27 00:18:57 -0500

    Day 22 Part 2

--- day22/combat.py
+++ day22/combat.py
@@ -1,18 +1,36 @@
 def play(player1: list, player2: list) -> int:
+    plays_cache = []
     while len(player1) != 0 and len(player2) != 0:
+        cards = {'1': player1.copy(), '2': player2.copy()}
+        if cards in plays_cache:
+            return (1, score(player2))
+        plays_cache.append(cards)
+
         card1 = player1.pop(0)
         card2 = player2.pop(0)
-        if card1 > card2:
-            player1.append(card1)
-            player1.append(card2)
+
+        if len(player1) >= card1 and len(player2) >= card2:
+            winner = play(player1.copy()[:card1], player2.copy()[:card2])[0]
+            if winner == 1:
+                player1.append(card1)
+                player1.append(card2)
+            else:
+                player2.append(card2)
+                player2.append(card1)
+
         else:
-            player2.append(card2)
-            player2.append(card1)
+            if card1 > card2:
+                player1.append(card1)
+                player1.append(card2)
+            else:
+                player2.append(card2)
+                player2.append(card1)
 
     if len(player1) == 0:
-        return score(player2)
+        return (2, score(player2))
     else:
-        return score(player1)
+        return (1, score(player1))
+
 
 def score(loser: list) -> int:
     winning_score = 0
--- day22/test_combat.py
+++ day22/test_combat.py
@@ -9,11 +9,17 @@ class MyTestCase(unittest.TestCase):
         player1 = [9, 2, 6, 3, 1]
         player2 = [5, 8, 4, 7, 10]
 
-        expected_winning_score = 306
+        expected_winning_score = 291
         winning_score = combat.play(player1, player2)
 
 
-        self.assertEqual(expected_winning_score, winning_score)
+        self.assertEqual(expected_winning_score, winning_score[1])
+
+
+    def test_infinite_game(self):
+        player1 = [43, 19]
+        player2 = [2, 29, 14]
+        combat.play(player1, player2)
 
 
 if __name__ == '__main__':
