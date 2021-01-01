55b27aa570e8490e9e4b525f04dcde6f98eeb119
Matt Porter <matthew.js.porter@gmail.com>
2021-01-01 15:28:23 -0500

    Day 23 Part 2

--- day23/test_cup.py
+++ day23/test_cup.py
@@ -1,24 +1,12 @@
 import unittest
 
-from day23 import crab
+from day23 import cup
 
 class MyTestCase(unittest.TestCase):
-    def test_10_rounds(self):
-        input = '389125467'
-        expected_order = '92658374'
-        order = crab.play(input, 10)
-        self.assertEqual(expected_order, order)
-
-    def test_100_rounds(self):
-        input = '389125467'
-        expected_order = '67384529'
-        order = crab.play(input, 100)
-        self.assertEqual(expected_order, order)
-
     def test_10_000_000(self):
         input = '389125467'
         expected = (934001, 159792)
-        cups = crab.play(input, 10000000)
+        cups = cup.play(input, 10000000)
         self.assertEqual(expected, cups)
 
 
